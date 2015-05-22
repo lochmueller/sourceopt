4a31d3a1991c2c5756d7c0306beea03d13ee49d7
Daniel Goerz <dlg@lightwerk.com>
2015-05-22 20:15:59 +0200

    Revert "[TASK][!!!] moved TypoScript to Configuration/TypoScript"
    This reverts commit 9cd68b6cbf29d4298e1772dc02881689bd04eee1.

--- ext_tables.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined("TYPO3_MODE")) {
-	die("Access denied.");
-}
-// Add static template configuration
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Sourceopt');
