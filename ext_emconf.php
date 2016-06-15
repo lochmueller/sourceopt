bac8b09b57c26ed4f9019745e0a2805e93a27bbf
Tim Lochmüller <tim@fruit-lab.de>
2016-06-15 21:37:25 +0200

    Format PSR2

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,20 +11,19 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array (
-	'title' => 'Source Optimization',
-	'description' => 'Optimization of the final page: reformatting the (x)html output, removal of new lines, and quotes. Move development repository to https://github.com/lochmueller/sourceopt',
-	'category' => 'fe',
-	'version' => '0.9.1',
-	'state' => 'beta',
-	'author' => 'Dr. Ronald P. Steiner, Boris Nicolai, Tim Lochmueller',
-	'author_email' => 'ronald.steiner@googlemail.com, boris.nicolai@andavida.com, tim@fruit-lab.de',
-	'author_company' => NULL,
-	'constraints' =>
-	array (
-		'depends' =>
-		array (
-			'typo3' => '6.2.0-7.99.99',
-		),
-	),
+    'title' => 'Source Optimization',
+    'description' => 'Optimization of the final page: reformatting the (x)html output, removal of new lines, and quotes. Move development repository to https://github.com/lochmueller/sourceopt',
+    'category' => 'fe',
+    'version' => '0.9.1',
+    'state' => 'beta',
+    'author' => 'Dr. Ronald P. Steiner, Boris Nicolai, Tim Lochmueller',
+    'author_email' => 'ronald.steiner@googlemail.com, boris.nicolai@andavida.com, tim@fruit-lab.de',
+    'author_company' => null,
+    'constraints' =>
+    array (
+        'depends' =>
+        array (
+            'typo3' => '6.2.0-7.99.99',
+        ),
+    ),
 );
-
--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined("TYPO3_MODE")) {
-	die("Access denied.");
+    die("Access denied.");
 }
 // Add static template configuration
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Sourceopt');
