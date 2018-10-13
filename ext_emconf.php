e24811c5903e1a8e0f73b3e8b8775a3a7d78b2c4
Rune Piper <kontakt@runepiper.de>
2018-10-13 12:37:31 +0200

    [FIX] Raise constraints for Environment
    Since commit 4f0449e and the introduction of the environment class we depend on TYPO3 9.5 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => null,
     'constraints' =>[
         'depends' => [
-            'typo3' => '8.7.0 - 9.5.99',
+            'typo3' => '9.5.0 - 9.5.99',
             'php' => '7.2',
         ],
     ],
