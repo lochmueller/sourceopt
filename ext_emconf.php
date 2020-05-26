8d4bab895e6b4103ad41a2eaba42ce779d2f8171
Tim Lochmüller <tim@fruit-lab.de>
2020-05-26 14:08:21 +0200

    Use secure versions of the core

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,8 +10,8 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => null,
     'constraints' =>[
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
-            'php' => '7.2.0-7.3.99',
+            'typo3' => '9.5.17-10.4.99',
+            'php' => '7.2.0-7.4.99',
         ],
     ],
 ];
