ab50285993df69f1cf18ac235de35d660084a2d0
Tim <tim@fruit-lab.de>
2021-11-17 21:05:11 +0100

    Add PHP CS Fixer

---
+++ Resources/Private/Build/PhpCsFixer.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+$baseDir = dirname(__DIR__, 3);
+require $baseDir.'/.Build/vendor/autoload.php';
+
+$finder = PhpCsFixer\Finder::create()
+    ->in($baseDir.'/Classes')
+    ->in($baseDir.'/Tests')
+    ->in($baseDir.'/Resources/Private/Build')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PhpCsFixer' => true,
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        '@DoctrineAnnotation' => true,
+        '@PSR2' => true,
+        '@PHP71Migration' => true,
+        '@PHP71Migration:risky' => true,
+        'array_syntax' => ['syntax' => 'short'],
+    ])
+    ->setFinder($finder)
+;
