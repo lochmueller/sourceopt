f6b4aef8931a81c88bba8d9e4bec67d143d03faf
Marcus Förster <m.foerster@xerc.de>
2022-06-09 15:14:07 +0200

    [OPT] Middleware Hook

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -1,17 +1,21 @@
 <?php
 
-use HTML\Sourceopt\Middleware\CleanHtmlMiddleware;
-
 return [
     'frontend' => [
         'html/sourceopt/clean-html' => [
-            'target' => CleanHtmlMiddleware::class,
+            'target' => \HTML\Sourceopt\Middleware\CleanHtmlMiddleware::class,
+            'before' => [
+                'typo3/cms-frontend/content-length-headers'
+            ],
             'after' => [
                 'typo3/cms-frontend/maintenance-mode',
             ],
         ],
         'html/sourceopt/svg-store' => [
             'target' => \HTML\Sourceopt\Middleware\SvgStoreMiddleware::class,
+            'before' => [
+                'typo3/cms-frontend/content-length-headers'
+            ],
             'after' => [
                 'html/sourceopt/clean-html',
             ]
